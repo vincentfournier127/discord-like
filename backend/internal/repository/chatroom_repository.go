bc50d8ea96f216681e9872504f1f41ac2c7bdb98
Vincent Fournier <genesisqc@hotmail.ca>
2026-05-19 20:11:47 -0400

    ChatroomRepo CreateChatroom

--- backend/internal/repository/chatroom_repository.go
+++ backend/internal/repository/chatroom_repository.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/jackc/pgx/v5/pgxpool"
+import (
+	"context"
+	"discord-like/internal/api/model"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type ChatroomRepository struct {
 	db *pgxpool.Pool
@@ -10,4 +15,21 @@ func NewChatroomRepository(db *pgxpool.Pool) *ChatroomRepository {
 	return &ChatroomRepository{
 		db: db,
 	}
+}
+
+func (r *ChatroomRepository) CreateChatroom(ctx context.Context, c *model.Chatroom) (*model.Chatroom, error) {
+	err := r.db.QueryRow(
+		ctx,
+		`INSERT INTO chatroom (name)
+		VALUES($1)
+		RETURNING id`,
+		c.Name,
+	).Scan(
+		&c.ID,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return c, nil
 }
